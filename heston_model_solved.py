9151c28b0ca9369b6b4a18b039d031d3eb516aee
Harvey Allen <57222485+harvey-allen@users.noreply.github.com>
2023-05-29 19:26:29 +0100

    heston model monte carlo simulation solution

--- heston_model_solved.py
+++ heston_model_solved.py
@@ -1,1 +1,42 @@
-""""""
+import numpy as np
+
+
+def heston_model_monte_carlo_simulation(s0, v0, rho, kappa, theta, sigma, t, n, m):
+    """
+    Inputs:
+     - S0, v0: Initial parameters for asset and variance.
+     - rho   : Correlation between asset returns and variance.
+     - kappa : Rate of mean reversion in variance process.
+     - theta : Long-term mean of variance process.
+     - sigma : Vol of vol / volatility of variance process.
+     - T     : Time of simulation.
+     - N     : Number of time steps.
+     - M     : Number of scenarios / simulations.
+    Outputs:
+    - Asset prices over time (numpy array).
+    - Variance over time (numpy array).
+    """
+
+    r = 0.02  # Risk-free rate
+
+    dt = t / n
+    mu = np.array([0, 0])
+    cov = np.array([[1, rho], [rho, 1]])
+
+    S = np.full(shape=(n + 1, m), fill_value=s0)
+    v = np.full(shape=(n + 1, m), fill_value=v0)
+
+    Z = np.random.multivariate_normal(mu, cov, (n, m))
+
+    for i in range(1, n + 1):
+        S[i] = S[i - 1] * np.exp(
+            (r - 0.5 * v[i - 1]) * dt + np.sqrt(v[i - 1] * dt) * Z[i - 1, :, 0]
+        )
+        v[i] = np.maximum(
+            v[i - 1]
+            + kappa * (theta - v[i - 1]) * dt
+            + sigma * np.sqrt(v[i - 1] * dt) * Z[i - 1, :, 1],
+            0,
+        )
+
+    return S, v
