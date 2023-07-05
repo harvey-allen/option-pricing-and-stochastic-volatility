5a1ac54f69d7e4a84c355ea54cd52a4731797335
Harvey Allen <57222485+harvey-allen@users.noreply.github.com>
2023-07-05 14:50:25 +0100

    Naming Conventions Fix

--- heston_model_solved.py
+++ heston_model_solved.py
@@ -14,30 +14,28 @@ def heston_model_monte_carlo_simulation(s0, v0, rho, kappa, theta, sigma, t, n, 
      - M: Number of scenarios / simulations.
      - R: Risk Free Rate
     Outputs:
-    - Asset prices over time (numpy array).
-    - Variance over time (numpy array).
+     - Asset prices over time (array).
+     - Variance over time (array).
     """
 
     dt = t / n
     mu = np.array([0, 0])
     cov = np.array([[1, rho], [rho, 1]])
-
     S = np.full(shape=(n + 1, m), fill_value=s0)
-    v = np.full(shape=(n + 1, m), fill_value=v0)
-
+    V = np.full(shape=(n + 1, m), fill_value=v0)
     Z = np.random.multivariate_normal(mu, cov, (n, m))
 
     for i in range(1, n + 1):
         S[i] = S[i - 1] * np.exp(
-            (r - 0.5 * v[i - 1]) * dt + np.sqrt(v[i - 1] * dt) * Z[i - 1, :, 0]
+            (r - 0.5 * V[i - 1]) * dt + np.sqrt(V[i - 1] * dt) * Z[i - 1, :, 0]
         )
-        v[i] = np.maximum(
-            v[i - 1]
-            + kappa * (theta - v[i - 1]) * dt
-            + sigma * np.sqrt(v[i - 1] * dt) * Z[i - 1, :, 1],
+        V[i] = np.maximum(
+            V[i - 1]
+            + kappa * (theta - V[i - 1]) * dt
+            + sigma * np.sqrt(V[i - 1] * dt) * Z[i - 1, :, 1],
             0,
         )
 
-    return S, v
+    return S, V
 
 
